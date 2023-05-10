30ddfafb55c7225242a4f219a942508188c9f2a8
Salman Saleem <37062860+yesmesalman@users.noreply.github.com>
2023-05-10 19:59:53 +0500

    task 1

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
-
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Workshop;
 
 class Event extends Model
 {
+    public function workshops()
+    {
+        return $this->hasMany(Workshop::class);
+    }
 }
